f368a55e624b8e3033c24bdd172dd4109da51003
Djibril-6et <djibril.6et@gmail.com>
2022-05-11 22:51:12 +0200

    small changes

--- suiviprojet/src/components/ProductCard.js
+++ suiviprojet/src/components/ProductCard.js
@@ -1,31 +1,29 @@
-import  React from 'react';
-import Button from '@mui/material/Button';
-
+import React from "react";
+import Button from "@mui/material/Button";
 
 export default function ProductCard(props) {
   return (
-     
     <div className="card items-center" key={props.product.id}>
+      <div className="bgcolor">
+        <img className="card-image justify-center" src={props.product.image} />
+      </div>
 
-    <img className='card-image justify-center ' src={props.product.image}/> 
-    
-    <h1>{props.product.title}</h1>
-    <p className="price">${props.product.price}</p>
-    <p className='overflow-hidden card_desc'>{props.product.description}</p>
-    {/* <Link href={`/shop/${props.product.id}`}> */}
-    {/* <div><Button text="See product" type="button" class="btn_card rounded_2"/></div> */}
-    {/* </Link> */}
+      <div className="product_data">
+        <h1>{props.product.title}</h1>
+        <div className="pricecolor">
+          <p className="price">${props.product.price}</p>
+        </div>
+        <p className="card_desc">{props.product.description}</p>
+        {/* <Link href={`/shop/${props.product.id}`}> */}
+        {/* <div><Button text="See product" type="button" class="btn_card rounded_2"/></div> */}
+        {/* </Link> */}
+      </div>
 
-
-   <Button  variant="contained" color='error' >
-        Add to Cart
-    </Button>
-
-</div>
-
-   
-
-
-
-  )
+      <div className="product_button">
+        <Button variant="contained" color="error">
+          Add to Cart
+        </Button>
+      </div>
+    </div>
+  );
 }
