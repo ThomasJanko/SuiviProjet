4d87aa7fd0b51ed0da402d5e81a94c4a213927ff
Djibril-6et <djibril.6et@gmail.com>
2022-05-13 00:38:32 +0200

    page with one product

--- suiviprojet/src/components/ProductCard.js
+++ suiviprojet/src/components/ProductCard.js
@@ -1,29 +1,35 @@
 import React from "react";
 import Button from "@mui/material/Button";
+import Link from "next/link";
 
 export default function ProductCard(props) {
   return (
-    <div className="card items-center" key={props.product.id}>
-      <div className="bgcolor">
-        <img className="card-image justify-center" src={props.product.image} />
-      </div>
-
-      <div className="product_data">
-        <h1>{props.product.title}</h1>
-        <div className="pricecolor">
-          <p className="price">${props.product.price}</p>
+    <Link href={`/${props.product.id}`}>
+      <div className="card items-center" key={props.product.id}>
+        <div className="bgcolor">
+          <img
+            className="card-image justify-center"
+            src={props.product.image}
+          />
         </div>
-        <p className="card_desc">{props.product.description}</p>
-        {/* <Link href={`/shop/${props.product.id}`}> */}
-        {/* <div><Button text="See product" type="button" class="btn_card rounded_2"/></div> */}
-        {/* </Link> */}
-      </div>
 
-      <div className="product_button">
-        <Button variant="contained" color="error">
-          Add to Cart
-        </Button>
+        <div className="product_data">
+          <h1>{props.product.title}</h1>
+          <div className="pricecolor">
+            <p className="price">${props.product.price}</p>
+          </div>
+          <p className="card_desc">{props.product.description}</p>
+          {/* <Link href={`/shop/${props.product.id}`}> */}
+          {/* <div><Button text="See product" type="button" class="btn_card rounded_2"/></div> */}
+          {/* </Link> */}
+        </div>
+
+        <div className="product_button">
+          <Button variant="contained" color="error">
+            Add to Cart
+          </Button>
+        </div>
       </div>
-    </div>
+    </Link>
   );
 }
--- suiviprojet/src/pages/[id]/index.js
+++ suiviprojet/src/pages/[id]/index.js
@@ -1,11 +1,52 @@
-import React from 'react';
+import React, { useState, useEffect } from "react";
+import { useRouter } from "next/router";
 
 const Index = () => {
-    return (
-        <div>
-            
+  const router = useRouter();
+  const [product, setProduct] = useState();
+
+  useEffect(() => {
+    const id = router.query.id;
+
+    fetch(`https://fakestoreapi.com/products/${id}`)
+      .then((res) => {
+        console.log(res);
+        return res.json();
+      })
+      .then((data) => {
+        console.log(data);
+        setProduct(data);
+      });
+  }, []);
+
+  return (
+    <>
+      <div className="one-product">
+        <div className="one-product__asset">
+          <img
+            className="one-product__asset__image"
+            src={product && product.image}
+          />
         </div>
-    );
-}
+        <span className="divider" />
+        <div className="one-product__data">
+          <h1 className="one-product__data__title">
+            {product && product.title}
+          </h1>
+          <p className="one-product__data__price">
+            {product && product.price} €
+          </p>
+          <p className="one-product__data__desc">
+            {product && product.description}
+          </p>
+
+          <button className="one-product__data__buttonADD">
+            Add to Cart
+          </button>
+        </div>
+      </div>
+    </>
+  );
+};
 
 export default Index;
