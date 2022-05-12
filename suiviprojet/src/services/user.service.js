ba047dee1150ca9db6e5a0e41b307a5628da6fd0
thomas78125 <thomas78125@gmail.com>
2022-05-13 01:06:09 +0200

    correction header

---
+++ suiviprojet/src/services/user.service.js
@@ -0,0 +1,28 @@
+const apiUrl = "http://localhost:1337/api";
+export default {
+	register(payload) {
+		return fetch(`${apiUrl}/auth/local/register`, {
+			method: "POST",
+			headers: {
+				"Content-Type": "Application/json",
+			},
+			body: JSON.stringify(payload),
+		}).then((res) => res.json());
+	},
+	login(payload) {
+		return fetch(`${apiUrl}/auth/local/`, {
+			method: "POST",
+			headers: {
+				"Content-Type": "Application/json",
+			},
+			body: JSON.stringify(payload),
+		}).then((res) => res.json());
+	},
+	getMe(jwt) {
+		return fetch(`${apiUrl}/users/me`, {
+			headers: {
+                "Authorization" : `Bearer ${jwt}`
+			},
+		}).then((res) => res.json());
+	},
+};
